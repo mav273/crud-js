76cef66abb31d5123eb074ec0961b8783a924663
mav273 <marcusvslourenco@fac.pe.senac.br>
2022-10-19 10:08:36 -0300

    Erro ao cadastrar usuário corrigido

--- classes/classes_usuarios.js
+++ classes/classes_usuarios.js
@@ -1,36 +1,43 @@
 const us = require("../sequelize/controllers/usuarios_controllers.js");
 
 class usuario {
-  constructor({username = null, senha}) {
+  constructor(username = null, senha) {
     this.username = username;
     this.senha = senha;
   }
   async login() {
-    try{
+    try {
       const id = await us.verificarUsuario(this.username, this.senha);
-      if (!isNaN(id)) {
+      if (id != null) {
         return id;
       } else {
         throw "Usuário ou Senha incorretos ou não cadastrados";
       }
-    } catch(e){ console.error(e) }
+    } catch (e) {
+      console.error(e);
+    }
   }
   async cadastrar() {
-    let ver = await us.verificarUsername(this.username);
-    console.log(ver);
-    if (isNaN(ver)) {
-      await us.criarUsuario(this.username, this.senha);
-    } else {
-      throw "Usuário já existe";
+    try {
+      let ver = await us.verificarUsername(this.username);
+      if (ver == null) {
+        await us.criarUsuario(this.username, this.senha);
+      } else {
+        throw "Usuário já existe";
+      }
+    } catch (e) {
+      console.error(e);
     }
   }
   async mudarsenha(user_id) {
-    try{
+    try {
       await us.atualizarSenha(user_id, this.senha);
-    } catch(e){ console.error(e) }
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
 
 module.exports = {
-  usuario
+  usuario,
 };
